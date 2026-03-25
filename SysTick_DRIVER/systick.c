ea241a06c8303df1e69f968cb9bdaedecffadb5c
Tanishk Yadav <sprabha430@gmail.com>
2026-03-25 21:37:37 +0530

    Update systick.c

--- SysTick_DRIVER/systick.c
+++ SysTick_DRIVER/systick.c
@@ -2,14 +2,14 @@
 #include "stm32l476xx.h"
 volatile uint16_t t;
 void systick_int(uint16_t ticks){
-    SysTick->CTRL=0;                    //*Disabling Control Register*//
-    SysTick->LOAD=ticks-1;              //*Frequency Setting Value and -1 for starting From 0*//
-    SysTick->VAL=0;                     //*Setting Counter Value*//
-    SCB->SHPR[11]=0xF0;                 //*Setting Interrupt priority to lowest, -11th in the Vector Table*//
-    SysTick->CTRL=7UL;                  //*Setting Control & Status Register (CLKSOURCE, TICK INT, ENABLE)*//
+    SysTick->CTRL=0; //*Disabling Control Register*//
+    SysTick->LOAD=ticks-1; //*Frequency Setting Value and -1 for starting From 0*//
+    SysTick->VAL=0; //*Assigning Counter Value*//
+    SCB->SHPR[11]=0xF0; //*Setting Interrupt priority to lowest, -11th in the Vector Table*//
+    SysTick->CTRL=7UL; //*Setting Control & Status Register (CLKSOURCE, TICK INT, ENABLE)*//
 }
-void SysTick_Handler(){                 //*SysTick Interrupt Handler*//
-	if(t>0)                             //*Counts down according to the Delay provided*//
+void SysTick_Handler(){ //*SysTick Interrupt Handler*//
+	if(t>0) //*Counts down according to the Delay argument*//
 		t--;                           
 }
 void delay(uint16_t time){
